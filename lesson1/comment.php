8ee1abeac54904383bfc38b7497fe39a6f865484
Ping <panfilo.glophics@gmail.com>
2021-09-29 19:41:59 +0800

    build

---
+++ lesson1/comment.php
@@ -0,0 +1,22 @@
+<?php //open tag
+
+// inline comment
+# inline comment
+
+/**
+ * block comment
+ * sdsd
+ * sdsd
+ */
+
+//.php extension
+//url browser
+//localhost, 127.0.0.1
+//localhost/folder name / filename
+//dispay text - echo, print
+// index.php or home.php
+
+//echo "hello world";
+// echo "echo text";
+//     print "printtext";
+// close tag ?>
